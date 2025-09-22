4ac195159cb2dd1aa5f48c4a82e2f2056be380be
dqdjgb <945847920@qq.com>
2025-09-22 15:46:57 +0800

    first

---
+++ sys/src/main/java/com/ll/sys/admin/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.ll.sys.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ll.sys.admin.domain.entity.MyUser;
+
+public interface UserMapper extends BaseMapper<MyUser> {
+}
---
+++ sys/src/main/java/com/ll/sys/admin/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ll.sys.admin.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ll.sys.admin.domain.entity.MyUser;
+import com.ll.sys.admin.mapper.UserMapper;
+
+public class UserServiceImpl extends ServiceImpl<UserMapper, MyUser> {
+}
